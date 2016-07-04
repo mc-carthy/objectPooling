b9372279568b36b1c411329e50decd62fce1488e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-04 08:23:40 +0100

    Create object spawning fountain

---
+++ Assets/Scripts/Stuff.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Stuff : MonoBehaviour {
+
+	public Rigidbody Body { get; private set; }
+
+	private void Awake () {
+		Body = GetComponent<Rigidbody> ();
+	}
+
+}
---
+++ Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class StuffSpawner : MonoBehaviour {
+
+	public float timeBetweenSpawns;
+	public float velocity;
+	public Stuff[] stuffPrefabs;
+
+	private float timeSinceLastSpawn;
+
+	private void FixedUpdate () {
+		timeSinceLastSpawn += Time.fixedDeltaTime;
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			timeSinceLastSpawn -= timeBetweenSpawns;
+			SpawnStuff ();
+		}
+	}
+
+	private void SpawnStuff () {
+		Stuff prefab = stuffPrefabs [Random.Range (0, stuffPrefabs.Length)];
+		Stuff spawn = Instantiate<Stuff> (prefab);
+		spawn.transform.localPosition = transform.position;
+		spawn.Body.velocity = transform.up * velocity;
+	}
+}
